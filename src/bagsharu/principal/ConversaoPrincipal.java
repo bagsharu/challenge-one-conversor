23c150241ce9f0ddff51516ccaf4fd2ab1caa075
Haru <gabs.haru@gmail.com>
2024-10-07 09:27:00 -0400

    Laço de repetição while e switch-case para rodar o programa

--- src/bagsharu/principal/ConversaoPrincipal.java
+++ src/bagsharu/principal/ConversaoPrincipal.java
@@ -13,13 +13,10 @@ public class ConversaoPrincipal {
         Scanner entrada = new Scanner(System.in);
         Menu myMenu = new Menu();
         ConsultarMoeda consultarMoeda = new ConsultarMoeda();
+        Info valores;
 
         System.out.println("Insira sua API Key para consultar");
         String KEY = entrada.nextLine();
-
-
-//
-
 
         int comando = 0;
         double valorConversao = 0;
@@ -35,12 +32,51 @@ public class ConversaoPrincipal {
                 case 1:
                     valorConversao = consultarMoeda.receberValor();
 
-                    Info valores = consultarMoeda.Consultar(KEY,"USD","BRL", valorConversao);
+                    valores = consultarMoeda.Consultar(KEY,"USD","BRL", valorConversao);
                     consultarMoeda.imprimirConversao(valores, valorConversao);
                     break;
                 // Real para Dólar
-//                case 2:
+                case 2:
+                    valorConversao = consultarMoeda.receberValor();
 
+                    valores = consultarMoeda.Consultar(KEY,"BRL","USD", valorConversao);
+                    consultarMoeda.imprimirConversao(valores, valorConversao);
+                    break;
+                // Dólar para Euro
+                case 3:
+                    valorConversao = consultarMoeda.receberValor();
+
+                    valores = consultarMoeda.Consultar(KEY,"USD","EUR", valorConversao);
+                    consultarMoeda.imprimirConversao(valores, valorConversao);
+                    break;
+                // Euro para Dólar
+                case 4:
+                    valorConversao = consultarMoeda.receberValor();
+
+                    valores = consultarMoeda.Consultar(KEY,"EUR","USD", valorConversao);
+                    consultarMoeda.imprimirConversao(valores, valorConversao);
+                    break;
+                // Real para Euro
+                case 5:
+                    valorConversao = consultarMoeda.receberValor();
+
+                    valores = consultarMoeda.Consultar(KEY,"BRL","EUR", valorConversao);
+                    consultarMoeda.imprimirConversao(valores, valorConversao);
+                    break;
+                // Euro para Real
+                case 6:
+                    valorConversao = consultarMoeda.receberValor();
+
+                    valores = consultarMoeda.Consultar(KEY,"EUR","BRL", valorConversao);
+                    consultarMoeda.imprimirConversao(valores, valorConversao);
+                    break;
+                // Saída
+                case -1:
+                    System.out.println("Encerrando operações...");
+                    entrada.close();
+                    break;
+                default:
+                    System.out.println("Parece que um valor inválido foi inserido, tente novamente");
             }
         }
 

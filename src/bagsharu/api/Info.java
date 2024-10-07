695ee7ac202c5dbe28f7363d0c9c6b1be1075e0d
Haru <gabs.haru@gmail.com>
2024-10-07 08:40:41 -0400

    Serialização de "conversion_result"

--- src/bagsharu/api/Info.java
+++ src/bagsharu/api/Info.java
@@ -2,5 +2,6 @@ package bagsharu.api;
 
 public record Info(String base_code,
                    String target_code,
-                   double conversion_rate) {
+                   double conversion_rate,
+                   double conversion_result) {
 }

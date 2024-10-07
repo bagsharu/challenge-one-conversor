55a650e38c429b6fd85d4f7d223e42e718ebb179
Haru <gabs.haru@gmail.com>
2024-10-07 09:00:28 -0400

    Formatação do menu

--- src/bagsharu/principal/ConversaoPrincipal.java
+++ src/bagsharu/principal/ConversaoPrincipal.java
@@ -4,47 +4,36 @@ import bagsharu.api.ConsultarMoeda;
 import bagsharu.api.Info;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.Scanner;
 
 public class ConversaoPrincipal {
     public static void main(String[] args) throws IOException, InterruptedException {
 
-
-//
-//        System.out.println(valores.base_code());
-//        System.out.println(valores.target_code());
-//        System.out.println(valores.conversion_rate());
-//        System.out.println(valores.conversion_result());
-//
-//        System.out.println(valores.BRL());
-//        System.out.println(valores.USD());
-//        System.out.println(valores.EUR());
-
         Scanner entrada = new Scanner(System.in);
         ConsultarMoeda consultarMoeda = new ConsultarMoeda();
-
-        System.out.println("Insira sua API Key para consultar");
-        String KEY = entrada.nextLine();
-
-        System.out.println("Digite o valor que deseja converter:");
-        double valorConversao = entrada.nextDouble();
-
-        Info valores = consultarMoeda.Consultar(KEY,"USD","BRL", valorConversao);
-        System.out.printf("O valor em %s %.2f corresponde ao valor aproximado de: %.2f %s",valores.base_code(), valorConversao, valores.conversion_result(), valores.target_code());
-
-//        int controle = 0;
 //
-//        while (controle != -1) {
+//        System.out.println("Insira sua API Key para consultar");
+//        String KEY = entrada.nextLine();
+
+//        System.out.println("Digite o valor que deseja converter:");
+//        double valorConversao = entrada.nextDouble();
 //
-//            switch (controle) {
-//
-//                case 1: // Dolar para Real
-//
-//            }
-//        }
+//        Info valores = consultarMoeda.Consultar(KEY,"USD","BRL", valorConversao);
+//        System.out.printf("O valor em %s %.2f corresponde ao valor aproximado de: %.2f %s",valores.base_code(), valorConversao, valores.conversion_result(), valores.target_code());
+
+        String menu = "\n ---------------------------------" +
+                      "\n Boas-vindas ao Conversor de Moedas!" +
+                      "\n " +
+                      "\n Escolha entre as opções válidas qual conversão deseja realizar!" +
+                      "\n 1) Dólar para Real" +
+                      "\n 2) Real para Dólar" +
+                      "\n 3) Dólar para Euro" +
+                      "\n 4) Euro para Dólar" +
+                      "\n 5) Real para Euro"  +
+                      "\n 6) Euro para Real"  +
+                      "\n Insira -1 para finalizar operações." +
+                      "\n ---------------------------------";
+
+        System.out.println(menu);
     }
 }

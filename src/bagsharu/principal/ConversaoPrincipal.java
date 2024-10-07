7365cf85f291529f9a8444b963bbc1e1975150f1
Haru <gabs.haru@gmail.com>
2024-10-07 09:07:15 -0400

    Estrutura de repetição para rodar o programa

--- src/bagsharu/principal/ConversaoPrincipal.java
+++ src/bagsharu/principal/ConversaoPrincipal.java
@@ -9,13 +9,11 @@ import java.util.Scanner;
 public class ConversaoPrincipal {
     public static void main(String[] args) throws IOException, InterruptedException {
 
+        // Instanciação de objetos
         Scanner entrada = new Scanner(System.in);
+        Menu myMenu = new Menu();
         ConsultarMoeda consultarMoeda = new ConsultarMoeda();
-        Menu menu = new Menu();
 
-        System.out.println(menu);
-
-//
 //        System.out.println("Insira sua API Key para consultar");
 //        String KEY = entrada.nextLine();
 
@@ -25,6 +23,14 @@ public class ConversaoPrincipal {
 //        Info valores = consultarMoeda.Consultar(KEY,"USD","BRL", valorConversao);
 //        System.out.printf("O valor em %s %.2f corresponde ao valor aproximado de: %.2f %s",valores.base_code(), valorConversao, valores.conversion_result(), valores.target_code());
 
+        int comando = 0;
+
+        // Laço de repetição while para "rodar" a aplicação
+        while (comando != -1) {
+            System.out.println(myMenu);
+            System.out.println("\n Digite qual operação deseja realizar");
+            comando = entrada.nextInt();
+        }
 
 
     }

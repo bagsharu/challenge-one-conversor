10f15558012a88d157cf69ccbfd5cd1f987fd5cb
Haru <gabs.haru@gmail.com>
2024-10-07 09:18:51 -0400

    Novos métodos de ConsultarMoeda para reutilização de código

--- src/bagsharu/principal/ConversaoPrincipal.java
+++ src/bagsharu/principal/ConversaoPrincipal.java
@@ -14,22 +14,34 @@ public class ConversaoPrincipal {
         Menu myMenu = new Menu();
         ConsultarMoeda consultarMoeda = new ConsultarMoeda();
 
-//        System.out.println("Insira sua API Key para consultar");
-//        String KEY = entrada.nextLine();
+        System.out.println("Insira sua API Key para consultar");
+        String KEY = entrada.nextLine();
 
-//        System.out.println("Digite o valor que deseja converter:");
-//        double valorConversao = entrada.nextDouble();
+
 //
-//        Info valores = consultarMoeda.Consultar(KEY,"USD","BRL", valorConversao);
-//        System.out.printf("O valor em %s %.2f corresponde ao valor aproximado de: %.2f %s",valores.base_code(), valorConversao, valores.conversion_result(), valores.target_code());
+
 
         int comando = 0;
+        double valorConversao = 0;
 
         // Laço de repetição while para "rodar" a aplicação
         while (comando != -1) {
             System.out.println(myMenu);
             System.out.println("\n Digite qual operação deseja realizar");
             comando = entrada.nextInt();
+
+            switch (comando) {
+                // Dólar para Real
+                case 1:
+                    valorConversao = consultarMoeda.receberValor();
+
+                    Info valores = consultarMoeda.Consultar(KEY,"USD","BRL", valorConversao);
+                    consultarMoeda.imprimirConversao(valores, valorConversao);
+                    break;
+                // Real para Dólar
+//                case 2:
+
+            }
         }
 
 

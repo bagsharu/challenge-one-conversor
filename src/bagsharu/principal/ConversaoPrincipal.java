15783a38ed1ed2c8564417f60f69126ab6c893d0
Haru <gabs.haru@gmail.com>
2024-10-07 08:25:33 -0400

    Implementação teste da consulta

--- src/bagsharu/principal/ConversaoPrincipal.java
+++ src/bagsharu/principal/ConversaoPrincipal.java
@@ -1,5 +1,8 @@
 package bagsharu.principal;
 
+import bagsharu.api.ConsultarMoeda;
+import bagsharu.api.Info;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -9,5 +12,16 @@ import java.net.http.HttpResponse;
 public class ConversaoPrincipal {
     public static void main(String[] args) throws IOException, InterruptedException {
 
+        ConsultarMoeda consultarMoeda = new ConsultarMoeda();
+
+        Info valores = consultarMoeda.Consultar("BRL","USD");
+
+        System.out.println(valores.base_code());
+        System.out.println(valores.target_code());
+        System.out.println(valores.conversion_rate());
+
+//        System.out.println(valores.BRL());
+//        System.out.println(valores.USD());
+//        System.out.println(valores.EUR());
     }
 }
